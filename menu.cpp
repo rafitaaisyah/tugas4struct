7e477a112222d37ca48a38ce9b3ab439507dc9cb
adeilyas <adeilyas@gmail.com>
2024-06-10 20:47:16 +0700

    add : daftar menu aplikasi

--- menu.cpp
+++ menu.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+void dMenu(){
+system("cls");
+cout<<"Aplikasi Tampilan Menu"<<"\n";       
+cout<<"1. Menu pertama"<<"\n";            
+cout<<"2. Menu kedua"<<"\n";            
+cout<<"3. Menu ketiga"<<"\n";           
+cout<<"4. Menu keempat"<<"\n";            
+cout<<"5. Exit"<<"\n";           
+cout<<"Masukan angka :";        
+
+}
+
 int main() {
-  cout << "Hello World!";
+dMenu();
   return 0;
 }
