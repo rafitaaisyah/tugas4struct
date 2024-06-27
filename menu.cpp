744077953c1038d91b93752990963708161005d8
adeilyas <adeilyas@gmail.com>
2024-06-27 12:02:35 +0800

    fix : penulisan huruf besar

--- menu.cpp
+++ menu.cpp
@@ -6,10 +6,10 @@ using namespace std;
 void dMenu(){
 system("cls");
 cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu pertama"<<"\n";            
-cout<<"2. Menu kedua"<<"\n";            
-cout<<"3. Menu ketiga"<<"\n";           
-cout<<"4. Menu keempat"<<"\n";            
+cout<<"1. Menu Pertama"<<"\n";            
+cout<<"2. Menu Kedua"<<"\n";            
+cout<<"3. Menu Ketiga"<<"\n";           
+cout<<"4. Menu Keempat"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
